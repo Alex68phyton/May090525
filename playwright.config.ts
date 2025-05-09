6a370c32304512e3d3a4225806824961ac4a4e1a
Alex <kapitan.galaktika@bk.ru>
2025-05-09 12:04:18 +0300

    first commit

---
+++ playwright.config.ts
@@ -0,0 +1,22 @@
+import { defineConfig, devices } from '@playwright/test';
+
+export default defineConfig({
+  testDir: './tests',
+  fullyParallel: true,
+  retries: process.env.CI ? 2 : 0,
+  workers: process.env.CI ? 1 : undefined,
+  reporter: 'html',
+  /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
+  use: {
+    trace: 'off',
+  },
+
+  /* Configure projects for major browsers */
+  projects: [
+    {
+      name: 'chromium',
+      use: { ...devices['Desktop Chrome'] },
+    }
+ 
+  ]
+});
---
+++ tests/first.test.ts
@@ -0,0 +1,7 @@
+import { test } from "@playwright/test";
+
+test.describe("Описание", async () => {
+    test("newtest", async () => {
+        console.log("ya napisal test");
+    })
+})
